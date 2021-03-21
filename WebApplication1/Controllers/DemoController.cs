87080cf696c059ea3527806c6f05544403810f9f
Jirapa <jirapa.jk34@gmail.com>
2021-03-21 14:17:05 +0700

    update

---
+++ WebApplication1/Controllers/DemoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        [Route("Name")]
+        [HttpGet]
+        public List<string> GetName(string name)
+        {
+            var list = new List<string>
+            {
+                "Nuchy",
+                "A",
+                "B",
+                "C"
+            };
+
+            if (name == null)
+            {
+                return list;
+            }
+
+            var results = list.Where(list => list.Contains(name));
+            return results.ToList();
+        }
+    }
+}
